32333d1fd7cb00157f3f936761fe52fa886f8a22
Andrei Voicu <Andrei.Voicu@softwire.com>
2026-06-24 14:34:20 +0300

    updated readme and added steps for using dapper

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,5 +1,7 @@
 using dotnet_bookish_starter.Models;
 using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace dotnet_bookish_starter.Controllers;
 
@@ -7,13 +9,20 @@ namespace dotnet_bookish_starter.Controllers;
 [Route("book")]
 public class BookController : ControllerBase
 {
+    private readonly string _connectionString;
+ 
+    public BookController(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("DbConnectionString") ?? "";
+    }
+
     [HttpGet]
-    public IEnumerable<Book> Get()
+    public async Task<IEnumerable<Book>> Get()
     {
         // TODO implement the GET method
         throw new NotImplementedException();
     }
-    
+
     [HttpPost]
     public Book Post([FromBody] Book book)
     {
